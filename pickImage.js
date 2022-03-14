d14ea7fde8cf7c8f98d17289edd70ce0383957e7
avinebel <73241397+avinebel@users.noreply.github.com>
2022-03-14 00:22:40 -0400

    Add files via upload

---
+++ pickImage.js
@@ -0,0 +1,32 @@
+// import images from "./imgInfo.js";
+
+// let todaysImage;
+// let todayNum = Math.floor(Math.random() * images.length)
+
+// if(images[todayNum].used === "true"){
+//     console.log("taken")
+// }else{
+//     todaysImage = images[todayNum];
+// }
+
+
+// document.getElementById("Pixelated").src = ""
+
+
+
+    // setInterval(() => {
+    //     let d = new Date()
+    //     let timeMilli = (d.getTime() - 1647187200000)/(1000*60*60);
+
+    //     //adam
+    //     let time = `${parseInt(timeMilli)}:${Math.floor((timeMilli - parseInt(timeMilli)) *60)}pm`;
+    //     console.log(timeMilli)
+    //     console.log(time)
+
+    //     //avi
+    //     let parsed = ((d.getTime() - 1647187200000)/(1000*60*60) + "").split(".")
+    //     console.log(parsed[1]*60)
+    //     console.log(parsed[0]+":"+(Math.floor((parsed[1].slice(0, 6)/1000000)*60)))
+    // }, 5000);
+
+    
